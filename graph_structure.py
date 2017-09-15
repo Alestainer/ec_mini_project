437879293e845f87c21fb2004f49235599c7a38f
alestainer <nikolaev.anton@phystech.edu>
2017-09-14 16:15:26 -1000

    Multiple vertices removal and add

--- graph_structure.py
+++ graph_structure.py
@@ -20,19 +20,36 @@ class Graph():
 		self.size = size
 
 		self.matrix = np.random.binomial(1, connectivity, size = (size, size))
+		self.matrix = np.fill_diagonal(self.matrix, 0)
 	
+	def drop_vertex(self, indices):
+		"""
+		Drop vertices from the matrix by index.
+		"""
+		self.matrix = np.delete(self.matrix, indices, axis = 0)
+		self.matrix = np.delete(self.matrix, indices, axis = 1)
+		self.size -= len(indices)
 
-	def drop_vertex(self, index):
-		
-		self.matrix = np.delete(self.matrix, index, axis = 0)
-		self.matrix = np.delete(self.matrix, index, axis = 1)
-		self.size -= 1
-
-
-	def add_vertex(self):
-		
+	def add_vertex(self, connectivity):
+		"""
+		Add vertex in the end of matrix, initialized with new connectivity.
+		"""
 		vector = np.random.binomial(1, connectivity, self.size) * .5
 		vector_2 = np.append(vector, [0])
 		
 		self.matrix = np.vstack((self.matrix, vector))
 		self.matrix = np.column_stack((self.matrix, vector_2))
+		self.size += 1
+
+	def add_vertices(self, num, connectivity):
+		"""
+		Add multiple vertices.
+		"""
+		for i in range(num):
+			self.add_vertex(connectivity)
+
+	def calculate_value(self):
+		"""
+		Calculate sum of connections for the vertices.
+		"""
+		return np.sum(self.matrix, axis = 1)
